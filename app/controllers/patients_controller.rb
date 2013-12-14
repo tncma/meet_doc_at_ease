d328b049e0b7cfc3bc8f52bc5183b91248a991a4
Prabha Pattabiraman <prabhap@thoughtworks.com>
2013-12-14 13:43:36 +0530

    Post action workinh

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,9 +1,16 @@
 class PatientsController < ApplicationController
+  skip_before_filter :verify_authenticity_token
+
   def create
     Patient.create(name: params[:name])
-    redirect_to show
+    render text: "hello"
   end
 
   def show
+  end
+
+  private
+  def permit_params
+    params.permit!
   end
 end
