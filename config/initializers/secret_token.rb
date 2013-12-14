20e5d583668368b12f95a42a985c2cdfc3dd3fbb
Abhinaya Sukumar <sabhinay@thoughtworks.com>
2013-12-14 12:16:04 +0530

    rails app setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MeetDocAtEase::Application.config.secret_token = '7cffb4b07d0192bdb8bd0b956c42c1a3a3e4cff9c67d154ce775811fa5abdc5c0661f0f98527d2ca7a41dbef7523ba46c787b1b618df13a39478888d5dbd39c3'
