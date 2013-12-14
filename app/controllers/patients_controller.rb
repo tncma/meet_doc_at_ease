b6a144c3d70c27dc98c2a165e851237a722eb86f
Prabha Pattabiraman <prabhap@thoughtworks.com>
2013-12-14 15:30:53 +0530

    creating patients in the show action

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -2,11 +2,14 @@ class PatientsController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
   def create
-    patient = Patient.create(name: params[:name])
+    patient = Patient.create!(name: params[:name])
     redirect_to patients_path(patient)
   end
 
   def show
+    p "#" * 100
+    p params
+    Patient.create!(name: params[:name])
   end
 
   private
