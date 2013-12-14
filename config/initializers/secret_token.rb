fc865b5e8d777a87590a458c8919061d39684127
Abhinaya Sukumar <sabhinay@thoughtworks.com>
2013-12-14 12:20:33 +0530

    Changing to rails 4

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,12 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-MeetDocAtEase::Application.config.secret_token = '7cffb4b07d0192bdb8bd0b956c42c1a3a3e4cff9c67d154ce775811fa5abdc5c0661f0f98527d2ca7a41dbef7523ba46c787b1b618df13a39478888d5dbd39c3'
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MeetDocAtEase::Application.config.secret_key_base = '42cbe51754ed66602a01676bbfb3db7129dbdf1ee7bbfb983c40cb0bee7ecaba9b16eb6871f544242b7104984e5a509ad2da8cbc78a3834a15d33dccec17d3f9'
