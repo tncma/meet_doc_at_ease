a46c5cfd028077ef58f6a91dfdd36a4c3c071709
Abhinaya Sukumar <sabhinay@thoughtworks.com>
2013-12-15 10:45:30 +0530

    showing route on map

--- app/assets/javascripts/map1.js
+++ app/assets/javascripts/map1.js
@@ -2,21 +2,23 @@ var Map = {
     _init: function() {
         this.map = new L.Map('map_leaflet');
         this.overlays = [];
-
-        var hull = new L.LatLng(13.8, 77);
+        var hull = new L.LatLng(latitude, longitude);
         var osmUrl='http://{s}.tile.cloudmade.com/d4fc77ea4a63471cab2423e66626cbb6/997/256/{z}/{x}/{y}.png';
         var osmAttrib='Map data © openstreetmap contributors';
         var osm = new L.TileLayer(osmUrl,{minZoom:6,maxZoom:18,attribution:osmAttrib});
-        this.map.setView(hull,8);
+        this.polyline = L.polyline([], {color: 'black'}).addTo(this.map);
+        this.map.setView(hull,14);
         this.map.addLayer(osm);
     },
     Add: function(index, lat, lun, size, color) {
-        color = color ? 'red' : color;
-        var circle = L.circle([lat, lun], 500 * size, {
-            color: color,
+        color = color ? 'blue' : color;
+        this.polyline.addLatLng([lat, lun])
+        var circle = L.circle([lat, lun], 40, {
+            color: "blue",
             fillColor: '#f03',
             fillOpacity: 0.3
         }).addTo(this.map);
+        circle.bindPopup("Ambulance is here");
         this.overlays[index] = circle;
     }
 }
@@ -27,7 +29,6 @@ $(function(){
         var index = evt.value;
             if (this.currentIndex != index) {
             var value = data[index];
-			console.log(value);
 	           if (value && value != "0") {
 	                Map.Add(index, value[0], value[1], value[2]);
 	           }
