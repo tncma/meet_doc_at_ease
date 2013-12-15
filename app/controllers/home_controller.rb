6ef54e579366a9a43e79458f8148243d1562f989
Prabha Pattabiraman <prabhap@thoughtworks.com>
2013-12-15 08:51:38 +0530

    Adding route information in the message

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def show
-    directions = Route.find(params[:route_id]).directions
+    directions = Route.find(params[:id]).directions
     @final_json = {}
     directions.each_with_index do |dis, index|
       @final_json[index] = [dis["lat"], dis["lng"], 1]
